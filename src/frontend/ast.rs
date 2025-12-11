4fb281f7b6291869d2a7a27c06a857edea131911
aqibfaruqui <aqibfaruqui@gmail.com>
2025-12-11 12:37:13 +0000

    feat(frontend): Update AST and Chumsky parser to support typestate syntax - Add support for assign statements and integer literal expressions - Add TypeState struct to capture ':: Type<InputState> -> Type<OutputState> signatures for verification - Update parser.rs for new syntax

---
+++ src/frontend/ast.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub functions: Vec<Function>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Function {
+    pub name: String,
+    pub args: Vec<String>,
+    pub signature: TypeState,
+    pub body: Vec<Statement>,
+}
+
+#[derive(Debug, Clone)]
+pub struct TypeState {
+    pub peripheral: String,
+    pub input_state: String,
+    pub output_state: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum Statement {
+    Let { var_name: String, value: Expr },
+    Assign { var_name: String, value, Expr },
+    Expr { expr: Expr },
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    IntLit {value: i32},            // TODO: Check if better as IntLit(i32) and Variable(String)
+    FnCall { name: String, args: Vec<Expr> },
+    Variable { name: String },
+}
---
+++ src/frontend/parser.rs
@@ -0,0 +1,123 @@
+use crate::ast;
+use chumsky::prelude::*;
+use chumsky::Parser;
+
+pub fn parse(source_code: &str) -> Result<ast::Program, Vec<chumsky::error::Simple<char>>> {
+    parser()
+        .parse(source_code)
+        .into_result()
+        .map_err(|errs| errs.into_iter().map(|e| e.into_simple()).collect())
+}
+
+fn parser<'src>() -> impl Parser<'src, &'src str, ast::Program, extra::Err<Simple<'src, char>>> {
+    // All of our 'atoms' (like identifiers, keywords, symbols)
+    // are '.padded()' to ignore whitespace around them.
+    let ident = text::ident().padded();
+    let int_lit = text::int(10).map(|s: &str| s.parse::<i32>().unwrap()).padded();
+    let comma = just(',').padded();
+
+    /* 
+     * Expression Parser 
+     * An expression atom is an IntLit, FnCall or Variable
+     */
+    let expr = recursive(|expr| {
+
+        let val = int_lit
+            .map(|value: i32| ast::Expr::Literal { value });
+
+        let fn_call = ident
+            .then(
+                expr.clone()
+                    .separated_by(comma)
+                    .allow_trailing()
+                    .collect()
+                    .delimited_by(just('(').padded(), just(')').padded()),
+            )
+            .map(|(name, args)| ast::Expr::FnCall { name, args });
+
+        let var = ident
+            .map(|name: String| ast::Expr::Variable { name });
+
+        val.or(fn_call).or(var)
+    });
+
+    /*
+     * Statement Parser 
+     * A statement atom is a Let, Assign or Expr
+     */
+    let statement = {
+
+        let let_stmt = text::keyword("let").padded()
+            .ignore_then(ident)
+            .then_ignore(just('=').padded())
+            .then(expr.clone())
+            .then_ignore(just(';').padded())
+            .map(|(var_name, value)| ast::Statement::Let { var_name, value });
+
+        let assign_stmt = ident
+            .then_ignore(just('=').padded())
+            .then(expr.clone())
+            .then_ignore(just(';').padded())
+            .map(|(var_name, value)| ast::Statement::Assign { var_name, value });
+
+        let expr_stmt = expr.clone()
+            .then_ignore(just(';').padded())
+            .map(|expr| ast::Statement::Expr { expr });
+
+        let_stmt.or(assign_stmt).or(expr_stmt)
+    };
+
+    /*
+     * Typestate Signature Parser
+     * :: Type<InputState> -> Type<OutputState>
+     */
+    let type_state = ident
+        .then(ident.delimited_by(just('<'), just('>')));
+
+    let signature = just("::").padded()
+        .ignore_then(type_state)
+        .then_ignore(just("->").padded())
+        .then(type_state)
+        .map(|((type_1, state_1), (type_2, state_2))| ast::TypestateSig {
+            peripheral: type_1,
+            input_state: state_1,
+            output_state: state_2
+        });
+
+    /* 
+     * Function Parser 
+     * 'fn func(arg1, arg2) :: Type<InputState> -> Type<OutputState> { 
+     *      statements 
+     *  }'
+     */
+    let function = text::keyword("fn").padded()
+        .ignore_then(ident)
+        .then(
+            ident
+                .separated_by(comma)
+                .allow_trailing()
+                .collect()
+                .delimited_by(just('(').padded(), just(')').padded()),
+        )
+        .then(signature)
+        .then(
+            statement
+                .repeated()
+                .collect()
+                .delimited_by(just('{').padded(), just('}').padded()),
+        )
+        .map(|(((name, args), signature), body)| ast::Function {
+            name,
+            args,
+            signature,
+            body,
+        });
+
+    /* Program Parser */
+    function
+        .padded()
+        .repeated()
+        .collect()
+        .map(|functions| ast::Program { functions })
+        .then_ignore(end());
+}
